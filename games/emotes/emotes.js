6e6b95a8cf9410fb053b15a1f22d829b995f1131
badoge <18620902+badoge@users.noreply.github.com>
2023-09-26 23:28:10 +0400

    Update homepage
    Also add guessr.tv to games selector :)

--- games/emotes/emotes.js
+++ games/emotes/emotes.js
@@ -333,43 +333,6 @@ function showHowToPlay() {
   howToPlayModal.show();
 } //showHowToPlay
 
-function switchGame(game) {
-  switch (game) {
-    case "draw":
-      location.href = "/games/draw";
-      break;
-    case "arena":
-      location.href = "/games/arena";
-      break;
-    case "eb":
-      location.href = "/games/emotes";
-      break;
-    case "dh":
-      location.href = "/games/donkhunt";
-      break;
-    case "shapes":
-      location.href = "/games/shapes";
-      break;
-    case "nim":
-      location.href = "/games/nim";
-      break;
-    case "nw":
-      location.href = "/games/wordle";
-      break;
-    case "c4":
-      location.href = "/games/connect4";
-      break;
-    case "ttt":
-      location.href = "/games/tictactoe";
-      break;
-    case "about":
-      aboutModal.show();
-      break;
-    default:
-      break;
-  }
-} //switchGame
-
 window.onload = async function () {
   darkTheme = (localStorage.getItem("darkTheme") || "true") === "true";
   elements.darkTheme.checked = darkTheme ?? true;
--- js/functions.js
+++ js/functions.js
@@ -502,6 +502,46 @@ function changeSiteLinkTarget(target) {
   }
 } //changeSiteLinkTarget
 
+function switchGame(game) {
+  switch (game) {
+    case "draw":
+      location.href = "/games/draw";
+      break;
+    case "arena":
+      location.href = "/games/arena";
+      break;
+    case "eb":
+      location.href = "/games/emotes";
+      break;
+    case "dh":
+      location.href = "/games/donkhunt";
+      break;
+    case "shapes":
+      location.href = "/games/shapes";
+      break;
+    case "nim":
+      location.href = "/games/nim";
+      break;
+    case "nw":
+      location.href = "/games/wordle";
+      break;
+    case "c4":
+      location.href = "/games/connect4";
+      break;
+    case "ttt":
+      location.href = "/games/tictactoe";
+      break;
+    case "guessr":
+      window.open("https://guessr.tv", "_blank").focus();
+      break;
+    case "about":
+      aboutModal.show();
+      break;
+    default:
+      break;
+  }
+} //switchGame
+
 async function getLinkInfo(element, allowThumbnails) {
   if (element.getAttribute("data-bs-title") == spinner) {
     let url = element.getAttribute("href");
