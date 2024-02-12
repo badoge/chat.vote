57d1b0c187d97faf814190b0e625286ae44ff47e
badoge <18620902+badoge@users.noreply.github.com>
2024-02-12 15:26:20 +0400

    add copy delete command button

--- playlist/playlist2.js
+++ playlist/playlist2.js
@@ -1,5 +1,6 @@
 let elements = {
   darkTheme: document.getElementById("darkTheme"),
+  toastContainer: document.getElementById("toastContainer"),
 };
 
 async function refreshData() {
@@ -17,6 +18,11 @@ function switchTheme(checkbox) {
   document.documentElement.setAttribute("data-bs-theme", checkbox ? "dark" : "light");
   document.getElementById("twitchLogo").style.filter = `invert(${checkbox ? 0.25 : 0.65})`;
 } //switchTheme
+
+function copyCommand(command) {
+  navigator.clipboard.writeText(command);
+  showToast("Command copied :)", "info", 1000);
+} //copyLink
 
 window.onload = function () {
   darkTheme = (localStorage.getItem("darkTheme") || "true") === "true";
