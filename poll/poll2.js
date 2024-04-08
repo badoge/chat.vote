e31bed588e10daf6495ff0442efa4dade8424943
badoge <18620902+badoge@users.noreply.github.com>
2024-04-08 22:27:59 +0400

    fix polls :)
    fixed toast not showing up when voting on polls that have results visibility other than everyone
    fixed chart sorting breaking when switching chart type
    chart is no longer shown if results are not visible

--- poll/poll2.js
+++ poll/poll2.js
@@ -171,84 +171,86 @@ async function vote() {
       showToast(result.message, "warning", 5000);
       return;
     }
+
+    showToast(result.message, "primary", 5000);
+
     unsortedData = structuredClone(result.data);
 
-    let total = 0;
     if (result.data.votes) {
-      total = result.data.votes.reduce((a, b) => a + b, 0) || 0;
-    }
-    elements.mainrow.innerHTML = `
-    <div class="col-xl-2"></div>
-    <div class="col-xl-8">
-      <div class="card bg-dark-subtle">
-        <div class="card-body">
-          <div id="countdowndiv"></div>
-          ${result.data.title == "Untitled poll" ? "" : "<h1 class='display-5'>" + validator.escape(result.data.title) + "</h1>"}
-          <div id="chartdiv" class="chart-container">
-            <canvas id="chartCanvas"></canvas>
-          </div>
-          <div class="container">
-            <div class="row mt-3">
-              <div class="col">
-                <div class="btn-group" role="group" aria-label="chart type toggles">
-                  <input class="btn-check" type="radio" name="charttype" id="barChart" autocomplete="off" checked />
-                  <label class="btn btn-outline-info" for="barChart" data-bs-toggle="tooltip" data-bs-placement="top" data-bs-title="Bar chart">
-                    <i style="transform: rotateZ(90deg)" class="material-icons notranslate">stacked_bar_chart</i>
-                  </label>
-                  <input class="btn-check" type="radio" name="charttype" id="pieChart" autocomplete="off" />
-                  <label class="btn btn-outline-info" for="pieChart" data-bs-toggle="tooltip" data-bs-placement="top" data-bs-title="Pie chart">
-                    <i class="material-icons notranslate">pie_chart</i>
-                  </label>
-                  </div>
-                  <input type="checkbox" class="btn-check" id="sortChart" autocomplete="off" ${sorted ? "checked" : ""}/>
-                  <label class="btn btn-outline-success" for="sortChart" id="sortChartLabel" data-bs-toggle="tooltip" data-bs-placement="top" data-bs-title="${
-                    sorted ? "Unsort chart" : "Sort chart"
-                  }"
-                    ><i class="material-icons notranslate">sort</i>
-                  </label>
-                  <div class="totalVotesDiv">Total votes: <span id="totalVotes">${total}</span></div>
-              </div>
-              <div class="col">
-              <button type="button" onclick="refresh()" class="float-end btn btn-info" data-bs-toggle="tooltip" data-bs-placement="top" data-bs-title="Refresh score">
-              <i class="material-icons notranslate">refresh</i>
-              </button>
-              <button type="button" id="backToPoll" onclick="backToPoll()" class="float-end btn btn-secondary" data-bs-toggle="tooltip"data-bs-placement="top" data-bs-title="Back to poll">
-              <i class="material-icons notranslate">arrow_back</i>
-              </button>
+      let total = result.data.votes.reduce((a, b) => a + b, 0) || 0;
+
+      elements.mainrow.innerHTML = `
+      <div class="col-xl-2"></div>
+      <div class="col-xl-8">
+        <div class="card bg-dark-subtle">
+          <div class="card-body">
+            <div id="countdowndiv"></div>
+            ${result.data.title == "Untitled poll" ? "" : "<h1 class='display-5'>" + validator.escape(result.data.title) + "</h1>"}
+            <div id="chartdiv" class="chart-container">
+              <canvas id="chartCanvas"></canvas>
+            </div>
+            <div class="container">
+              <div class="row mt-3">
+                <div class="col">
+                  <div class="btn-group" role="group" aria-label="chart type toggles">
+                    <input class="btn-check" type="radio" name="charttype" id="barChart" autocomplete="off" checked />
+                    <label class="btn btn-outline-info" for="barChart" data-bs-toggle="tooltip" data-bs-placement="top" data-bs-title="Bar chart">
+                      <i style="transform: rotateZ(90deg)" class="material-icons notranslate">stacked_bar_chart</i>
+                    </label>
+                    <input class="btn-check" type="radio" name="charttype" id="pieChart" autocomplete="off" />
+                    <label class="btn btn-outline-info" for="pieChart" data-bs-toggle="tooltip" data-bs-placement="top" data-bs-title="Pie chart">
+                      <i class="material-icons notranslate">pie_chart</i>
+                    </label>
+                    </div>
+                    <input type="checkbox" class="btn-check" id="sortChart" autocomplete="off" ${sorted ? "checked" : ""}/>
+                    <label class="btn btn-outline-success" for="sortChart" id="sortChartLabel" data-bs-toggle="tooltip" data-bs-placement="top" data-bs-title="${
+                      sorted ? "Unsort chart" : "Sort chart"
+                    }"
+                      ><i class="material-icons notranslate">sort</i>
+                    </label>
+                    <div class="totalVotesDiv">Total votes: <span id="totalVotes">${total}</span></div>
+                </div>
+                <div class="col">
+                <button type="button" onclick="refresh()" class="float-end btn btn-info" data-bs-toggle="tooltip" data-bs-placement="top" data-bs-title="Refresh score">
+                <i class="material-icons notranslate">refresh</i>
+                </button>
+                <button type="button" id="backToPoll" onclick="backToPoll()" class="float-end btn btn-secondary" data-bs-toggle="tooltip"data-bs-placement="top" data-bs-title="Back to poll">
+                <i class="material-icons notranslate">arrow_back</i>
+                </button>
+                </div>
               </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
-    <div class="col-xl-2"></div>`;
-    if (result.data.endTime && parseInt(result.data.endTime, 10) - Date.now() / 1000 > 0) {
-      showCountdown(result.data.endTime);
+      <div class="col-xl-2"></div>`;
+      if (result.data.endTime && parseInt(result.data.endTime, 10) - Date.now() / 1000 > 0) {
+        showCountdown(result.data.endTime);
+      }
+
+      loadChart();
+
+      document.getElementById("barChart").onchange = function () {
+        barChart = this.checked;
+        pieChart = !this.checked;
+        loadChart();
+      };
+      document.getElementById("pieChart").onchange = function () {
+        pieChart = this.checked;
+        barChart = !this.checked;
+        loadChart();
+      };
+      document.getElementById("sortChart").addEventListener(
+        "click",
+        function () {
+          sorted = this.checked;
+          const tooltip = bootstrap.Tooltip.getInstance("#sortChartLabel");
+          tooltip.setContent({ ".tooltip-inner": this.checked ? "Unsort chart" : "Sort chart" });
+          loadChart();
+        },
+        false
+      );
     }
-
-    loadChart(result.data.questions, result.data.scores);
-
-    document.getElementById("barChart").onchange = function () {
-      barChart = this.checked;
-      pieChart = !this.checked;
-      loadChart(result.data.questions, result.data.scores);
-    };
-    document.getElementById("pieChart").onchange = function () {
-      pieChart = this.checked;
-      barChart = !this.checked;
-      loadChart(result.data.questions, result.data.scores);
-    };
-    document.getElementById("sortChart").addEventListener(
-      "click",
-      function () {
-        sorted = this.checked;
-        const tooltip = bootstrap.Tooltip.getInstance("#sortChartLabel");
-        tooltip.setContent({ ".tooltip-inner": this.checked ? "Unsort chart" : "Sort chart" });
-        loadChart(result.data.questions, result.data.scores);
-      },
-      false
-    );
-    showToast(result.message, "primary", 5000);
   } catch (error) {
     if (!token) {
       captchaModal.show();
@@ -284,80 +286,85 @@ async function refresh() {
     };
     let response = await fetch(`https://poll.chat.vote/api/results/${pollID}/${token}`, requestOptions);
     let result = await response.json();
+    console.log(result);
+    showToast(result.message, "primary", 5000);
     unsortedData = structuredClone(result.data);
 
-    elements.mainrow.innerHTML = `
-    <div class="col-xl-2"></div>
-    <div class="col-xl-8">
-      <div class="card bg-dark-subtle">
-        <div class="card-body">
-          <div id="countdowndiv"></div>
-          ${result.data.title == "Untitled poll" ? "" : "<h1 class='display-5'>" + validator.escape(result.data.title) + "</h1>"}
-          <div id="chartdiv" class="chart-container">
-            <canvas id="chartCanvas"></canvas>
-          </div>
-          <div class="container">
-            <div class="row mt-3">
-              <div class="col">
-                <div class="btn-group" role="group" aria-label="chart type toggles">
-                  <input class="btn-check" type="radio" name="charttype" id="barChart" autocomplete="off" checked />
-                  <label class="btn btn-outline-info" for="barChart" data-bs-toggle="tooltip" data-bs-placement="top" data-bs-title="Bar chart">
-                    <i style="transform: rotateZ(90deg)" class="material-icons notranslate">stacked_bar_chart</i>
-                  </label>
-                  <input class="btn-check" type="radio" name="charttype" id="pieChart" autocomplete="off" />
-                  <label class="btn btn-outline-info" for="pieChart" data-bs-toggle="tooltip" data-bs-placement="top" data-bs-title="Pie chart">
-                    <i class="material-icons notranslate">pie_chart</i>
-                  </label>
-                  </div>
-                  <input type="checkbox" class="btn-check" id="sortChart" autocomplete="off" ${sorted ? "checked" : ""}/>
-                  <label class="btn btn-outline-success" for="sortChart" id="sortChartLabel" data-bs-toggle="tooltip" data-bs-placement="top" data-bs-title="${
-                    sorted ? "Unsort chart" : "Sort chart"
-                  }"
-                    ><i class="material-icons notranslate">sort</i>
-                  </label>
-                  <div class="totalVotesDiv">Total votes: <span id="totalVotes">${result.data.votes[0]}</span></div>
-              </div>
-              <div class="col">
-              <button type="button" onclick="refresh()" class="float-end btn btn-info" data-bs-toggle="tooltip" data-bs-placement="top" data-bs-title="Refresh score">
-              <i class="material-icons notranslate">refresh</i>
-              </button>
-              <button type="button" id="backToPoll" onclick="backToPoll()" class="float-end btn btn-secondary" data-bs-toggle="tooltip" data-bs-placement="top" data-bs-title="Back to poll">
-              <i class="material-icons notranslate">arrow_back</i>
-              </button>
+    if (result.data.scores) {
+      let total = result.data.votes.reduce((a, b) => a + b, 0) || 0;
+
+      elements.mainrow.innerHTML = `
+      <div class="col-xl-2"></div>
+      <div class="col-xl-8">
+        <div class="card bg-dark-subtle">
+          <div class="card-body">
+            <div id="countdowndiv"></div>
+            ${result.data.title == "Untitled poll" ? "" : "<h1 class='display-5'>" + validator.escape(result.data.title) + "</h1>"}
+            <div id="chartdiv" class="chart-container">
+              <canvas id="chartCanvas"></canvas>
+            </div>
+            <div class="container">
+              <div class="row mt-3">
+                <div class="col">
+                  <div class="btn-group" role="group" aria-label="chart type toggles">
+                    <input class="btn-check" type="radio" name="charttype" id="barChart" autocomplete="off" checked />
+                    <label class="btn btn-outline-info" for="barChart" data-bs-toggle="tooltip" data-bs-placement="top" data-bs-title="Bar chart">
+                      <i style="transform: rotateZ(90deg)" class="material-icons notranslate">stacked_bar_chart</i>
+                    </label>
+                    <input class="btn-check" type="radio" name="charttype" id="pieChart" autocomplete="off" />
+                    <label class="btn btn-outline-info" for="pieChart" data-bs-toggle="tooltip" data-bs-placement="top" data-bs-title="Pie chart">
+                      <i class="material-icons notranslate">pie_chart</i>
+                    </label>
+                    </div>
+                    <input type="checkbox" class="btn-check" id="sortChart" autocomplete="off" ${sorted ? "checked" : ""}/>
+                    <label class="btn btn-outline-success" for="sortChart" id="sortChartLabel" data-bs-toggle="tooltip" data-bs-placement="top" data-bs-title="${
+                      sorted ? "Unsort chart" : "Sort chart"
+                    }"
+                      ><i class="material-icons notranslate">sort</i>
+                    </label>
+                    <div class="totalVotesDiv">Total votes: <span id="totalVotes">${total}</span></div>
+                </div>
+                <div class="col">
+                <button type="button" onclick="refresh()" class="float-end btn btn-info" data-bs-toggle="tooltip" data-bs-placement="top" data-bs-title="Refresh score">
+                <i class="material-icons notranslate">refresh</i>
+                </button>
+                <button type="button" id="backToPoll" onclick="backToPoll()" class="float-end btn btn-secondary" data-bs-toggle="tooltip" data-bs-placement="top" data-bs-title="Back to poll">
+                <i class="material-icons notranslate">arrow_back</i>
+                </button>
+                </div>
               </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
-    <div class="col-xl-2"></div>`;
-    if (result.data.endTime && parseInt(result.data.endTime, 10) - Date.now() / 1000 > 0) {
-      showCountdown(result.data.endTime);
+      <div class="col-xl-2"></div>`;
+      if (result.data.endTime && parseInt(result.data.endTime, 10) - Date.now() / 1000 > 0) {
+        showCountdown(result.data.endTime);
+      }
+
+      loadChart();
+
+      document.getElementById("barChart").onchange = function () {
+        barChart = this.checked;
+        pieChart = !this.checked;
+        loadChart();
+      };
+      document.getElementById("pieChart").onchange = function () {
+        pieChart = this.checked;
+        barChart = !this.checked;
+        loadChart();
+      };
+      document.getElementById("sortChart").addEventListener(
+        "click",
+        function () {
+          sorted = this.checked;
+          const tooltip = bootstrap.Tooltip.getInstance("#sortChartLabel");
+          tooltip.setContent({ ".tooltip-inner": this.checked ? "Unsort chart" : "Sort chart" });
+          loadChart();
+        },
+        false
+      );
     }
-
-    loadChart(result.data.questions, result.data.scores);
-
-    document.getElementById("barChart").onchange = function () {
-      barChart = this.checked;
-      pieChart = !this.checked;
-      loadChart(result.data.questions, result.data.scores);
-    };
-    document.getElementById("pieChart").onchange = function () {
-      pieChart = this.checked;
-      barChart = !this.checked;
-      loadChart(result.data.questions, result.data.scores);
-    };
-    document.getElementById("sortChart").addEventListener(
-      "click",
-      function () {
-        sorted = this.checked;
-        const tooltip = bootstrap.Tooltip.getInstance("#sortChartLabel");
-        tooltip.setContent({ ".tooltip-inner": this.checked ? "Unsort chart" : "Sort chart" });
-        loadChart(result.data.questions, result.data.scores);
-      },
-      false
-    );
-    showToast(result.message, "primary", 5000);
   } catch (error) {
     if (!token) {
       captchaModal.show();
@@ -369,43 +376,37 @@ async function refresh() {
   linkifyElementID("mainrow", true);
 } //refresh
 
-function loadChart(questions, scores) {
-  console.log(questions);
-  console.log(scores);
-
+function loadChart() {
   let ctx = document.getElementById("chartCanvas").getContext("2d");
   let options = [];
+  let labels = [];
 
-  scores = scores[0];
+  let scores = structuredClone(unsortedData.scores);
+  let questions = structuredClone(unsortedData.questions);
+
+  let total = scores.reduce((a, b) => a + b, 0) || 0;
 
   if (sorted) {
     let list = [];
+
     for (let i = 0, j = questions[0].options.length; i < j; i++) {
-      list.push({ option: questions[0].options[i], score: scores[i] });
+      list.push({ option: questions[0].options[i], score: scores[0][i] });
     }
+
     list.sort(function (a, b) {
       return a.score > b.score ? -1 : a.score == b.score ? 0 : 1;
     });
+
     for (let i = 0, j = questions[0].options.length; i < j; i++) {
       options[i] = list[i].option;
-      scores[i] = list[i].score;
+      scores[0][i] = list[i].score;
     }
   } else {
-    options = unsortedData.questions[0].options;
-    scores = unsortedData.scores[0];
+    options = questions[0].options;
   }
-
-  let total = 0;
-  if (scores) {
-    total = scores.reduce((a, b) => a + b, 0) || 0;
-  } else {
-    scores = [];
-  }
-
-  let labels = [];
 
   for (let index = 0, j = questions[0].options.length; index < j; index++) {
-    let score = !scores[index] ? 0 : scores[index];
+    let score = !scores[0][index] ? 0 : scores[0][index];
     labels[index] = `${options[index].name} - ${score} ${score == 1 ? "Vote" : "Votes"} (${Math.round((score / total) * 100) || 0}%)`;
   }
 
@@ -422,7 +423,7 @@ function loadChart(questions, scores) {
         datasets: [
           {
             label: "Score",
-            data: scores,
+            data: scores[0],
             borderWidth: 2,
             backgroundColor: colors,
             borderColor: colors,
@@ -476,7 +477,7 @@ function loadChart(questions, scores) {
         datasets: [
           {
             label: "Score",
-            data: scores,
+            data: scores[0],
             borderWidth: 2,
             backgroundColor: colors,
             borderColor: colors,
