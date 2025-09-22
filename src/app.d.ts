008f6b7aa10a808c4cd75b8456dbc03f7ea3901c
badoge <18620902+badoge@users.noreply.github.com>
2025-09-22 23:42:38 +0400

    move stuff around

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,5 +1,7 @@
 // See https://svelte.dev/docs/kit/types#app.d.ts
 // for information about these interfaces
+import 'unplugin-icons/types/svelte';
+
 declare global {
 	namespace App {
 		// interface Error {}
@@ -7,6 +9,14 @@ declare global {
 		// interface PageData {}
 		// interface PageState {}
 		// interface Platform {}
+	}
+}
+
+declare global {
+	namespace Twitch {
+		export class Embed {
+			constructor(divId: string, options: TwitchEmbedOptions);
+		}
 	}
 }
 
