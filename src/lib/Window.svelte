ffa60fd1b94dc8a15f84794e0805486d4cdd1864
badoge <18620902+badoge@users.noreply.github.com>
2025-12-30 04:35:06 +0400

    windows test

---
+++ src/lib/Window.svelte
@@ -0,0 +1,216 @@
+<script>
+  import { onMount } from "svelte";
+
+  import { createDraggable } from "animejs";
+
+  onMount(() => {
+    createDraggable(".window", {
+      trigger: ".window-header",
+      container: "body",
+      containerPadding: [64, 0, 32, 0],
+    });
+  });
+</script>
+
+<div class="window">
+  <div class="window-header-bg"></div>
+  <header class="window-header">
+    <img
+      src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAA8UlEQVR42pWSsQ2DMBBFrYiNbhZqqngFWlfs4G0YwHVWQKIKZZTC8b/kHHMYSCx9YYHf88dgTDH6vo/DMJzG7A0Izob3PrZtG48Fz/tuIMC1KtECYwOnFAAkohylcBkqYbIxLsvC8xACt8BmqoWLNj3waQHRO4ljicC4zvOcJYXApRtY7leVITHhKwGMTNONJVnA7x48V9WvICnhlQBwSLtTWiQCaSDVAWIOcNOg6zqub+jdABnHkcNnwVK/38A58zk0m4EyGq6eAcDL9RGbpuEAktTgjUAk8iPpz6bhqkC+huxYAho+FODBr6kKcPOfvACzZogqWb89XgAAAABJRU5ErkJggg=="
+      alt="Internet Explorer"
+      class="header-icon"
+      draggable="false"
+    />
+    <div class="header-title">dank</div>
+    <div class="header-buttons">
+      <button class="header-minimize" title="minimize"></button>
+      <button class="header-maximize" title="maximize"></button>
+      <button class="header-close" title="close"></button>
+    </div>
+  </header>
+  <div class="window-content">
+    <div class="window-stuff">dank</div>
+  </div>
+</div>
+
+<style>
+  .window {
+    width: 700px;
+    height: 500px;
+    display: flex;
+    position: absolute;
+    padding: 3px;
+    background-color: rgb(8, 49, 217);
+    flex-direction: column;
+    border-top-left-radius: 8px;
+    border-top-right-radius: 8px;
+  }
+
+  .window-content {
+    flex: 1 1 0%;
+    position: relative;
+    margin-top: 25px;
+    height: calc(100% - 25px);
+  }
+
+  .window-stuff {
+    height: 100%;
+    width: 100%;
+    position: absolute;
+    display: flex;
+    overflow: hidden;
+    flex-direction: column;
+    background: linear-gradient(to right, rgb(237, 237, 229) 0%, rgb(237, 232, 205) 100%);
+  }
+
+  .window-header-bg {
+    user-select: none;
+    background: linear-gradient(
+      rgb(0, 88, 238) 0%,
+      rgb(53, 147, 255) 4%,
+      rgb(40, 142, 255) 6%,
+      rgb(18, 125, 255) 8%,
+      rgb(3, 111, 252) 10%,
+      rgb(2, 98, 238) 14%,
+      rgb(0, 87, 229) 20%,
+      rgb(0, 84, 227) 24%,
+      rgb(0, 85, 235) 56%,
+      rgb(0, 91, 245) 66%,
+      rgb(2, 106, 254) 76%,
+      rgb(0, 98, 239) 86%,
+      rgb(0, 82, 214) 92%,
+      rgb(0, 64, 171) 94%,
+      rgb(0, 48, 146) 100%
+    );
+    position: absolute;
+    left: 0px;
+    top: 0px;
+    right: 0px;
+    height: 28px;
+    pointer-events: none;
+    border-top-left-radius: 8px;
+    border-top-right-radius: 8px;
+    overflow: hidden;
+  }
+
+  .window-header {
+    user-select: none;
+    display: flex;
+    height: 25px;
+    line-height: 25px;
+    font-weight: 700;
+    font-size: 12px;
+    font-family: "Noto Sans";
+    text-shadow: rgb(0, 0, 0) 1px 1px;
+    color: white;
+    position: absolute;
+    left: 3px;
+    right: 3px;
+    -webkit-box-align: center;
+    align-items: center;
+  }
+
+  .header-icon {
+    width: 15px;
+    height: 15px;
+    margin-left: 1px;
+    margin-right: 3px;
+  }
+
+  .header-title {
+    flex: 1 1 0%;
+    pointer-events: none;
+    padding-right: 5px;
+    letter-spacing: 0.5px;
+    overflow: hidden;
+    white-space: nowrap;
+    text-overflow: ellipsis;
+  }
+
+  .header-buttons {
+    opacity: 1;
+    height: 22px;
+    display: flex;
+    -webkit-box-align: center;
+    align-items: center;
+    margin-top: -1px;
+    margin-right: 1px;
+  }
+
+  .header-buttons > button {
+    margin-right: 1px;
+    position: relative;
+    width: 22px;
+    height: 22px;
+    border: 1px solid rgb(255, 255, 255);
+    border-radius: 3px;
+  }
+
+  .header-buttons > button:hover {
+    filter: brightness(120%);
+  }
+
+  .header-buttons > button:active {
+    filter: brightness(90%);
+  }
+
+  .header-minimize {
+    box-shadow: rgb(70, 70, 255) 0px -1px 2px 1px inset;
+    background-image: radial-gradient(circle at 90% 90%, rgb(0, 84, 233) 0%, rgb(34, 99, 213) 55%, rgb(68, 121, 228) 70%, rgb(163, 187, 236) 90%, white 100%);
+  }
+
+  .header-minimize::before {
+    content: "";
+    position: absolute;
+    left: 4px;
+    top: 13px;
+    height: 3px;
+    width: 8px;
+    background-color: white;
+  }
+
+  .header-maximize {
+    box-shadow: rgb(70, 70, 255) 0px -1px 2px 1px inset;
+    background-image: radial-gradient(circle at 90% 90%, rgb(0, 84, 233) 0%, rgb(34, 99, 213) 55%, rgb(68, 121, 228) 70%, rgb(163, 187, 236) 90%, white 100%);
+  }
+
+  .header-maximize::before {
+    content: "";
+    position: absolute;
+    display: block;
+    left: 4px;
+    top: 4px;
+    box-shadow:
+      white 0px 3px inset,
+      white 0px 0px 0px 1px inset;
+    height: 12px;
+    width: 12px;
+  }
+
+  .header-close {
+    box-shadow: rgb(218, 70, 0) 0px -1px 2px 1px inset;
+    background-image: radial-gradient(circle at 90% 90%, rgb(204, 70, 0) 0%, rgb(220, 101, 39) 55%, rgb(205, 117, 70) 70%, rgb(255, 204, 178) 90%, white 100%);
+  }
+
+  .header-close::before {
+    content: "";
+    position: absolute;
+    left: 9px;
+    top: 2px;
+    transform: rotate(45deg);
+    height: 16px;
+    width: 2px;
+    background-color: white;
+  }
+
+  .header-close::after {
+    content: "";
+    position: absolute;
+    left: 9px;
+    top: 2px;
+    transform: rotate(-45deg);
+    height: 16px;
+    width: 2px;
+    background-color: white;
+  }
+</style>
--- src/routes/games/+layout.svelte
+++ src/routes/games/+layout.svelte
@@ -5,8 +5,25 @@
   import IcRoundOpenInNew from "~icons/ic/round-open-in-new";
   import IcBaselineClose from "~icons/ic/baseline-close";
   import HowToPlay from "$lib/HowToPlay.svelte";
+  import Window from "$lib/Window.svelte";
 
   let { children } = $props();
+
+  let games = [
+    { name: "draw", title: "Draw", description: "Streamer draws a random emote, chat has to guess the emote. Can you draw well enough?" },
+    { name: "arena", title: "Arena", description: 'Fight your chatters in a "battle royale" arena, where only one can win!' },
+    { name: "eb", title: "Emote benchmark", description: "A test of reaction speed and emote knowledge. Type the appearing emotes in chat as fast as you can." },
+    { name: "dh", title: "Donk Hunt", description: "Scary looking creatures are trying to trap their prey. Are you the hunter or the hunted one?" },
+    { name: "shapes", title: "🟥⏹️🔴🔴⭕⏹️", description: "A very weird logic puzzle. Finish the row of shapes, which has been formed using a pre-determined hidden rule." },
+    { name: "nim", title: "Nim", description: "Classic. Remove popsicles until there's one left. Whoever takes the last one - loses!" },
+    { name: "nw", title: "Wordle", description: "A twist of a well-known game: try to guess a word in several attempts. Your chat will choose the hidden word." },
+    { name: "c4", title: "Connect 4", description: "Players take turns to drop their pieces into the container, attempting to connect 4 of their pieces in a row." },
+    { name: "ttt", title: "tic tac toe", description: "An ancient game of wits. Will you outsmart the hive mind - which is your chat?" },
+    { name: "guessr", title: "Guessr.tv", description: "Guess the view count. You will be presented with a random Twitch stream and you have to guess how many viewers they have." },
+  ];
+
+  let now = $state(new Date());
+  setInterval(() => (now = new Date()), 60000);
 </script>
 
 <dialog id="howToPlayModal" class="modal">
@@ -65,106 +82,135 @@
 
 <Navbar />
 
-<div class="grid grid-cols-4 gap-4">
-  <div class="col">
-    <div class="card card-border bg-base-200">
-      <img src="/gamespics/draw.png" onclick={() => switchGame("draw")} class="rounded-t-2xl" alt="Draw" />
-      <div class="card-body">
-        <h5 class="card-title">Draw</h5>
-        <p class="card-text">Streamer draws a random emote, chat has to guess the emote. Can you draw well enough?</p>
+<div class="desktop">
+  <Window />
+  <footer class="taskbar">
+    <button class="px-2 flex rounded-e-lg bg-gradient-to-b from-[#4cc552] to-[#2e8b33] border border-[#1f6f28] shadow-inner text-white font-bold">
+      <span class="text-xl flex gap-1 items-center italic"><img class="h-5" src="/pics/donk.png" alt="donk" />Start</span>
+    </button>
+
+    <div class="taskbar-items">
+      {#each games as game}
+        <div class="taskbar-item">
+          <img class="footer__icon h-5" src="/pics/donk.png" alt={game.title} />
+          <div class="footer__text">{game.title}</div>
+        </div>
+      {/each}
+    </div>
+
+    <div class="taskbar-icons">
+      <div class="clock">
+        {now.toLocaleTimeString([], { hour: "2-digit", minute: "2-digit" })}
       </div>
     </div>
-  </div>
-  <div class="col">
-    <div class="card card-border bg-base-200">
-      <img src="/gamespics/arena.png" onclick={() => switchGame("arena")} class="rounded-t-2xl" alt="Arena" />
-      <div class="card-body">
-        <h5 class="card-title">Arena</h5>
-        <p class="card-text">Fight your chatters in a "battle royale" arena, where only one can win!</p>
-      </div>
-    </div>
-  </div>
-  <div class="col">
-    <div class="card card-border bg-base-200">
-      <img src="/gamespics/eb.png" onclick={() => switchGame("eb")} class="rounded-t-2xl" alt="Emote benchmark" />
-      <div class="card-body">
-        <h5 class="card-title">Emote benchmark</h5>
-        <p class="card-text">A test of reaction speed and emote knowledge. Type the appearing emotes in chat as fast as you can.</p>
-      </div>
-    </div>
-  </div>
-  <div class="col">
-    <div class="card card-border bg-base-200">
-      <img src="/gamespics/dh.png" onclick={() => switchGame("dh")} class="rounded-t-2xl" alt="Donk Hunt" />
-      <div class="card-body">
-        <h5 class="card-title">Donk Hunt</h5>
-        <p class="card-text">Scary looking creatures are trying to trap their prey. Are you the hunter or the hunted one?</p>
-      </div>
-    </div>
-  </div>
-  <div class="col">
-    <div class="card card-border bg-base-200">
-      <img src="/gamespics/shapes.png" onclick={() => switchGame("shapes")} class="rounded-t-2xl" alt="🟥⏹️🔴🔴⭕⏹️" />
-      <div class="card-body">
-        <h5 class="card-title">🟥⏹️🔴🔴⭕⏹️</h5>
-        <p class="card-text">A very weird logic puzzle. Finish the row of shapes, which has been formed using a pre-determined hidden rule.</p>
-      </div>
-    </div>
-  </div>
-  <div class="col">
-    <div class="card card-border bg-base-200">
-      <img src="/gamespics/nim.png" onclick={() => switchGame("nim")} class="rounded-t-2xl" alt="Nim" />
-      <div class="card-body">
-        <h5 class="card-title">Nim</h5>
-        <p class="card-text">Classic. Remove popsicles until there's one left. Whoever takes the last one - loses!</p>
-      </div>
-    </div>
-  </div>
-  <div class="col">
-    <div class="card card-border bg-base-200">
-      <img src="/gamespics/nw.png" onclick={() => switchGame("nw")} class="rounded-t-2xl" alt="Not Wordle :)" />
-      <div class="card-body">
-        <h5 class="card-title">Not Wordle :)</h5>
-        <p class="card-text">A twist of a well-known game: try to guess a word in several attempts. Your chat will choose the hidden word.</p>
-      </div>
-    </div>
-  </div>
-  <div class="col">
-    <div class="card card-border bg-base-200">
-      <img src="/gamespics/c4.png" onclick={() => switchGame("c4")} class="rounded-t-2xl" alt="Connect 4" />
-      <div class="card-body">
-        <h5 class="card-title">Connect 4</h5>
-        <p class="card-text">Players take turns to drop their pieces into the container, attempting to connect 4 of their pieces in a row.</p>
-      </div>
-    </div>
-  </div>
-  <div class="col">
-    <div class="card card-border bg-base-200">
-      <img src="/gamespics/ttt.png" onclick={() => switchGame("ttt")} class="rounded-t-2xl" alt="tic tac toe" />
-      <div class="card-body">
-        <h5 class="card-title">tic tac toe</h5>
-        <p class="card-text">An ancient game of wits. Will you outsmart the hive mind - which is your chat?</p>
-      </div>
-    </div>
-  </div>
-  <div class="col">
-    <div class="card card-border bg-base-200">
-      <img src="/gamespics/guessr.png" onclick={() => switchGame("guessr")} class="rounded-t-2xl" alt="guessr" />
-      <div class="card-body">
-        <h5 class="card-title"><IcRoundOpenInNew /> Guessr.tv</h5>
-        <p class="card-text">Guess the view count. You will be presented with a random Twitch stream and you have to guess how many viewers they have.</p>
-      </div>
-    </div>
-  </div>
-  <div class="col">
-    <div class="card card-border bg-base-200">
-      <img src="/pics/donk.png" style="width: 180px; height: 180px; align-self: center" onclick={() => aboutModal.showModal()} class="rounded-t-2xl" alt="About" />
-      <div class="card-body">
-        <h5 class="card-title">About</h5>
-        <p class="card-text">About section</p>
-      </div>
-    </div>
-  </div>
+  </footer>
 </div>
 
 {@render children?.()}
+
+<style>
+  .desktop {
+    height: calc(100vh - 64px);
+    font-family: Tahoma, "Noto Sans", sans-serif;
+    overflow: hidden;
+    position: relative;
+    background: url("/background.png") center center / cover no-repeat fixed;
+    animation: 5s ease 0s 1 normal forwards running none;
+  }
+
+  .taskbar {
+    height: 30px;
+    background: linear-gradient(
+      rgb(31, 47, 134) 0px,
+      rgb(49, 101, 196) 3%,
+      rgb(54, 130, 229) 6%,
+      rgb(68, 144, 230) 10%,
+      rgb(56, 131, 229) 12%,
+      rgb(43, 113, 224) 15%,
+      rgb(38, 99, 218) 18%,
+      rgb(35, 91, 214) 20%,
+      rgb(34, 88, 213) 23%,
+      rgb(33, 87, 214) 38%,
+      rgb(36, 93, 219) 54%,
+      rgb(37, 98, 223) 86%,
+      rgb(36, 95, 220) 89%,
+      rgb(33, 88, 212) 92%,
+      rgb(29, 78, 192) 95%,
+      rgb(25, 65, 165) 98%
+    );
+    position: fixed;
+    bottom: 0px;
+    right: 0px;
+    left: 0px;
+    display: flex;
+  }
+
+  .clock {
+    margin: 0px 5px;
+    color: rgb(255, 255, 255);
+    font-size: 11px;
+    font-weight: lighter;
+    text-shadow: none;
+  }
+
+  .taskbar-items {
+    display: flex;
+    -webkit-box-align: center;
+    align-items: center;
+
+    height: 100%;
+    flex: 1 1 0%;
+    overflow: hidden;
+  }
+
+  .taskbar-item {
+    flex: 1 1 0%;
+    max-width: 150px;
+    color: rgb(255, 255, 255);
+    border-radius: 2px;
+    margin-top: 2px;
+    padding: 0px 8px;
+    height: 22px;
+    font-size: 11px;
+    background-color: rgb(60, 129, 243);
+    box-shadow:
+      rgba(0, 0, 0, 0.3) -1px 0px inset,
+      rgba(255, 255, 255, 0.2) 1px 1px 1px inset;
+    position: relative;
+    display: flex;
+    -webkit-box-align: center;
+    align-items: center;
+  }
+
+  .taskbar-icons {
+    flex-shrink: 0;
+    background: linear-gradient(
+      rgb(12, 89, 185) 1%,
+      rgb(19, 158, 233) 6%,
+      rgb(24, 181, 242) 10%,
+      rgb(19, 155, 235) 14%,
+      rgb(18, 144, 232) 19%,
+      rgb(13, 141, 234) 63%,
+      rgb(13, 159, 241) 81%,
+      rgb(15, 158, 237) 88%,
+      rgb(17, 155, 233) 91%,
+      rgb(19, 146, 226) 94%,
+      rgb(19, 126, 215) 97%,
+      rgb(9, 91, 201) 100%
+    );
+    border-left: 1px solid rgb(16, 66, 175);
+    box-shadow: rgb(24, 187, 255) 1px 0px 1px inset;
+    padding: 0px 10px;
+    margin-left: 10px;
+    display: flex;
+    -webkit-box-align: center;
+    align-items: center;
+  }
+
+  .taskbar-item.active {
+    background-color: rgb(30, 82, 183);
+    box-shadow:
+      rgba(0, 0, 0, 0.2) 0px 0px 1px 1px inset,
+      rgba(0, 0, 0, 0.7) 1px 0px 1px inset;
+  }
+</style>
